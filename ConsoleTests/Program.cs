e2b186fcd01547010b00e78d27fd77733b0a0088
José Pablo Cortés <jpcortesp@hotmail.com>
2015-11-25 18:13:21 -0600

    Lazy Experiment en ConsoleTests

--- ConsoleTests/Program.cs
+++ ConsoleTests/Program.cs
@@ -10,20 +10,23 @@ namespace ConsoleTests
     {
         static void Main(string[] args)
         {
-            var Manifest = new Api.Manifest.OnlineManifest();
-            var api = new Api.API(Manifest, "6def2424db3a4a8db1cef0a2c3a7807e");
-            var player = api.getPlayerAsync(new Api.Objects.BungieUser() { GamerTag = "jpcortesp", type = Api.Objects.MembershipType.Xbox }).Result;
+            //var Manifest = new Api.Manifest.OnlineManifest();
+            //var api = new Api.API(Manifest, "6def2424db3a4a8db1cef0a2c3a7807e");
+            //var player = api.getPlayerAsync(new Api.Objects.BungieUser() { GamerTag = "jpcortesp", type = Api.Objects.MembershipType.Xbox }).Result;
 
-            foreach (var item in player.Gear)
-            {
-                Console.WriteLine("({0} / {1}) - {2}", item.state, item.transferStatus, item.itemTypeName);
-            }
+            //foreach (var item in player.Gear)
+            //{
+            //    Console.WriteLine("({0} / {1}) - {2}", item.state, item.transferStatus, item.itemTypeName);
+            //}
             //foreach (var item in player.Characters)
             //{
             //    Console.WriteLine("{0} - {1} ({2})",item.Class, item.LightLevel, item.BaseLevel);
             //    Console.WriteLine(item.Race + " " + item.Gender);
             //}
 
+            var player = new Player(new object(), new object());
+            Console.Write(player.Items.Count);
+            
             Console.ReadLine();
             
 
