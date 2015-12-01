ddb5e7c88069ba45d806bc19991c4e2643d509fc
José Pablo Cortés <jpcortesp@hotmail.com>
2015-12-01 12:47:06 -0600

    Adding nuspec

--- Api/Properties/AssemblyInfo.cs
+++ Api/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DestinyPCL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A PCL client for Destiny API (Bungie Game)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("JPCortesP")]
 [assembly: AssemblyProduct("Api")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
