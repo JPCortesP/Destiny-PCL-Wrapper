c5c03b303676bef6af40363b09c1dfe73ae69d94
José Pablo Cortés <jpcortesp@hotmail.com>
2015-12-05 16:10:00 -0600

    Adding Proto Win32 Manifest

---
+++ AlternativeManifests/Win32Manifest/Manifest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DestinyPCL.Manifest;
+
+namespace DestinyPCLWin32Manifest
+{
+    public class Manifest : DestinyPCL.DestinyManifest
+    {
+        public string ApiKey        {            get; set;        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic getData(ManifestTable table, string hash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<dynamic> getDataAsync(ManifestTable table, string hash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Preload()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
