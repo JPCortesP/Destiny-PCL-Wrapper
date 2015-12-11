73625c7b5fc255af57659edf842505a6834be4b3
José Pablo Cortés <jpcortesp@hotmail.com>
2015-12-11 17:39:12 -0600

    Changes to demo, about to merge with master

--- Examples/ConsoleTests/Program.cs
+++ Examples/ConsoleTests/Program.cs
@@ -23,9 +23,17 @@ namespace ConsoleTests
             var api = new DestinyPCL.DestinyService(new Win32Manifest(), "6def2424db3a4a8db1cef0a2c3a7807e");
             var player = api.getPlayerAsync(user).Result;
 
-            foreach (var item in player.Gear)
+            var ItemTypes = player.Gear.Select(g => g.itemTypeName).Distinct();
+            foreach (var type in ItemTypes)
             {
-                Console.WriteLine("{0} - {1} {2}", item.itemName,item.itemTypeName, item.primaryStats_value);
+                Console.WriteLine("Top {0}s", type);
+                Console.WriteLine("=====================");
+                foreach (var item in player.Gear.Where(g=>g.itemTypeName == type).OrderByDescending(h=>h.primaryStats_value).Take(2))
+                {
+                    Console.WriteLine("{0} - {1}", item.itemName, item.primaryStats_value);
+                }
+                Console.WriteLine("{0} more {1}", player.Gear.Where(g=>g.itemTypeName == type).Count() -2, type);
+                Console.WriteLine("");
             }
 
             Console.ReadLine();
